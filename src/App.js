c2636406b1978361ad554972583352cc86b16ad3
LAPTOP-2E9KHGK5\34704 <347046045@qq.com>
2022-01-29 16:29:13 +0800

    优化拖拽交互体验，useEffect添加useRef变量

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 /* 布局 菜单可有可无，菜单宽度可拖拽 */
 
 function App() {
-  const storage = +localStorage.getItem("storageWidth");
+  let firstUpdate = useRef(true);
+
+  const storage = Number(localStorage.getItem("storageWidth"));
   const [count, setCount] = useState(0);
   const [x, setX] = useState(0);
+  const [toStart, setToStart] = useState(false);
   const [start, setStart] = useState(false);
   const [menuWidth, setMenuWidth] = useState(storage || 100);
+  const dragPadding = 50;
   const setStorage = (name, value) => {
     const has = localStorage.getItem(name);
     if (has) {
@@ -18,52 +22,75 @@ function App() {
       localStorage.setItem(value, name);
     }
   };
+
+  useEffect(() => {
+    if (firstUpdate.current) {
+      console.log(firstUpdate.current, "???");
+      firstUpdate.current = false;
+      return;
+    }
+    setMenuWidth((menuWidth) => {
+      setStorage(x + 30, "storageWidth");
+      console.log("执行了");
+      return x + 30;
+    });
+  }, [x]);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      onMouseUp={(event) => {
+        setStart(() => false);
+      }}
+    >
       <header className="App-header">
-        <div
-          className="header"
-          onClick={() => {
-            setMenuWidth((menuWidth) => 100);
-          }}
-        >
-          banner
-        </div>
+        <div className="header">banner</div>
         <div className="content-wrapper">
           <div
             className="menu"
             style={{
               width: menuWidth + "px",
               display: menuWidth < 40 ? "none" : "block",
+              cursor: toStart ? "e-resize" : "auto",
             }}
             onClick={() => {
               setCount((count) => count + 1);
             }}
             onMouseDown={(event) => {
-              setStart(() => true);
+              if (menuWidth - event.pageX < dragPadding) {
+                setStart(() => true);
+              }
             }}
             onMouseMove={(event) => {
+              if (menuWidth - event.pageX < dragPadding) {
+                setToStart((toStart) => true);
+              } else {
+                setToStart((toStart) => false);
+              }
               if (start) {
                 console.log(event, "move");
                 setX((preventX) => {
-                  return event.movementX;
-                });
-                setMenuWidth((menuWidth) => {
-                  setStorage(menuWidth + x, "storageWidth");
-                  return (menuWidth = menuWidth + x);
+                  return event.pageX;
                 });
               }
             }}
-            onMouseUp={(event) => {
-              setStart(() => false);
-            }}
-            onMouseOut={(event) => {
-              setStart(() => false);
-            }}
           >
-            菜单{count}
+            可拖拽 改变宽度 菜单 点击次数
+            {count}
           </div>
-          <div className="content">内容</div>
+          <div className="content">
+            内容
+            <button
+              onClick={() => {
+                setMenuWidth((menuWidth) => {
+                  setStorage(menuWidth < 40 ? 100 : 39, "storageWidth");
+                  return menuWidth < 40 ? 100 : 39;
+                });
+              }}
+            >
+              {menuWidth < 40 ? "点击显示菜单" : "隐藏菜单"}
+            </button>
+          </div>
         </div>
         <div className="footer">底部</div>
       </header>
